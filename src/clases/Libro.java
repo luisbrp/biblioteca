5e00dd253c6ec12fa2e1248fbf566981f84639ad
Luis Fernando Mosquera <Luis Fernando Mosquera@HZ114820.H012108.NET>
2023-01-26 12:55:11 +0100

    Generados los getters y setters + mas la clase GestorBiblioteca

--- src/clases/Libro.java
+++ src/clases/Libro.java
@@ -5,4 +5,30 @@ public class Libro {
 	private String titulo;
 	private String autor;
 	private int num_pag;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getNum_pag() {
+		return num_pag;
+	}
+	public void setNum_pag(int num_pag) {
+		this.num_pag = num_pag;
+	}
+	
+	
 }
