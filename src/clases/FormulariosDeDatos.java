96b8ca14c40c61fd9a4db58cd88111c9986a8f6e
Luis Fernando Mosquera <Luis Fernando Mosquera@HZ114820.H012108.NET>
2023-01-31 09:30:02 +0100

    Hechas pedirDatosLibro, modificarDatosLibro y pedirIdLibro.

--- src/clases/FormulariosDeDatos.java
+++ src/clases/FormulariosDeDatos.java
@@ -5,15 +5,41 @@ import java.util.Scanner;
 public class FormulariosDeDatos {
 	
 	public static Libro pedirDatosLibro(Scanner scan) {
-		return null;
+		
+		Libro libro = new Libro();
+		
+		System.out.println("Introduce el id del libro:");
+		libro.setId(Integer.parseInt(scan.nextLine()));
+		
+		System.out.println("Introduce el titulo del libro:");
+		libro.setTitulo(scan.nextLine());
+		
+		System.out.println("Introduce el autor del libro:");
+		libro.setAutor(scan.nextLine());
+		
+		System.out.println("Introduce el numero de la pagina del libro:");
+		libro.setNum_pag(Integer.parseInt(scan.nextLine()));
+		
+		return libro;
 	}
 	
 	public static Libro modificarDatosLibro(Libro libro, Scanner scan) {
-		return libro;
 		
+		System.out.println("Introduce el id del libro:");
+
+		libro.setId(Integer.parseInt(scan.nextLine()));
+		
+		return libro;
 	}
 	
 	public static int pedirIdLibro(Scanner scan) {
-		return 0;
+		Libro libro = new Libro();
+		
+		System.out.println("Introduce el id del libro");
+		libro.setId(Integer.parseInt(scan.nextLine()));
+		
+		int id = libro.getId();
+		
+		return id;
 	}
 }
