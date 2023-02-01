72a369f4125f73debec2fb3ff34b03f724af9010
Luis Fernando Mosquera <Luis Fernando Mosquera@HZ114820.H012108.NET>
2023-02-01 13:36:37 +0100

    Creado el menu en la clase GestorBiblioteca

--- src/clases/GestorBiblioteca.java
+++ src/clases/GestorBiblioteca.java
@@ -1,8 +1,37 @@
 package clases;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class GestorBiblioteca {
 	
-	public void run () {
+	public void run () throws SQLException {
+		int opcion;
+		GestorLibros gl = new GestorLibros();
+		GestorSocios gs = new GestorSocios();
+		do {
+			Menu.mostrarMenuPrincipal();;
+			Scanner scan =  new Scanner(System.in);
+			opcion=Integer.parseInt(scan.nextLine());
+
+			switch (opcion) {
+			case Menu.GESTIONAR_LIBROS:
+				gl.run(scan);
+				break;
+			case Menu.GESTIONAR_SOCIOS:
+				
+				break;
+			case Menu.GESTIONAR_PRESTAMOS:
+				
+				break;
+			case Menu.SALIR:
+				System.out.println("Bye!!!");
+				break;
+			default:
+				System.out.println("Opcion incorrecta!");
+			}
+
+		} while (opcion != Menu.SALIR);
 	
 	}
 }
