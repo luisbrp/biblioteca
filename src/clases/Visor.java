fee58b531bf2dea4c693fa73be2d45bcba9d01ae
Luis Fernando Mosquera <Luis Fernando Mosquera@HZ114820.H012108.NET>
2023-02-02 13:49:23 +0100

    Hecho el metodo de mostrar libros en visor

--- src/clases/Visor.java
+++ src/clases/Visor.java
@@ -1,11 +1,19 @@
 package clases;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Visor {
 	
 	
 	public static void mostrarLibros(ArrayList<Libro> libros) {
+		
+			System.out.println("Lista de libros:");
+			for (int i = 0; i < libros.size(); i++) {
+				Libro libro = libros.get(i);
+				System.out.println(libro.toString());
+				
+			}
 		
 	}
 	
