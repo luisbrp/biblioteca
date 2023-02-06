c586d62a08c8418158e7d60882b2cb43eedd88c1
Luis Fernando Mosquera <Luis Fernando Mosquera@HZ114820.H012108.NET>
2023-02-06 14:11:49 +0100

    Clase GestorBBDD terminada

--- src/clases/GestorBBDD.java
+++ src/clases/GestorBBDD.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.sql.Date;
 import java.util.Scanner;
 
 public class GestorBBDD extends Conector {
@@ -110,14 +111,19 @@ public class GestorBBDD extends Conector {
 		preparedSt.setInt(7, socio.getDni());
 		
 		preparedSt.execute();
+		
+		System.out.println("El socio se ha introducido con exito!");
 	}
 	
 	public void eliminarSocio (int id) throws SQLException {
 	
-		preparedSt = con.prepareStatement("DELETE FROM socios WHERE id = ? ;");
+		preparedSt = con.prepareStatement("DELETE FROM socios WHERE id=? ;");
 
 		preparedSt.setInt(1, id);
+		
 		preparedSt.execute();
+		
+		System.out.println("El socio se ha sido eliminado con exito!");
 	}
 	
 	public void modificarSocio(Socio socio) throws SQLException {
@@ -134,6 +140,8 @@ public class GestorBBDD extends Conector {
 		preparedSt.setInt(7, socio.getDni());
 		
 		preparedSt.execute();
+		
+		System.out.println("El socio se ha sido modificado con exito!");
 	}
 	
 	public Socio getSocio (int id) throws SQLException  {
@@ -159,5 +167,85 @@ public class GestorBBDD extends Conector {
 		return socio;
 		
 	}
+	
+	public ArrayList<Socio> getSocios() throws SQLException{
+		
+		ArrayList<Socio> socios = new ArrayList<Socio>();
+		
+		preparedSt = con.prepareStatement("SELECT * FROM socios");
+
+		ResultSet resultado = preparedSt.executeQuery();
+		
+		while(resultado.next()) {
+			
+			Socio socio = new Socio();
+			
+			socio.setId(resultado.getInt(1));
+			socio.setNombre(resultado.getString(2));
+			socio.setApellido(resultado.getString(3));
+			socio.setDireccion(resultado.getString(4));
+			socio.setPoblacion(resultado.getString(5));
+			socio.setProvincia(resultado.getString(6));
+			socio.setDni(resultado.getInt(7));
+			
+			socios.add(socio);
+			
+		}
+
+		return socios;
+		
+	}
+	
+	public void insertPrestamo(Prestamo prestamo) throws SQLException {
+
+
+			preparedSt = con.prepareStatement("INSERT INTO prestamos(id_libro, id_socio, fecha, devuelto) VALUES (?, ?,?,?)");
+			preparedSt.setInt(1, prestamo.getId_libro());
+			preparedSt.setInt(2, prestamo.getId_socio());
+			
+		
+			preparedSt.setDate(3, new java.sql.Date(prestamo.getFecha().getTime()));
+			preparedSt.setBoolean(4, prestamo.getDevuelto());
+			
+			preparedSt.execute();
+
+		System.out.println("El prestamo se ha realizado con exito!");
+	}
+
+	
+	
+	public void devolverLibro(int idLibro) throws SQLException {
+		
+			Prestamo prestamo = prestamoNoDevuelto(idLibro);
+	
+			preparedSt= con.prepareStatement("UPDATE prestamos SET devuelto=true WHERE id_libro =? and id_socio=? and fecha=?");
+			preparedSt.setInt(1, prestamo.getId_libro());
+			preparedSt.setInt(2, prestamo.getId_socio());
+			preparedSt.setDate(3,(java.sql.Date) new Date(prestamo.getFecha().getTime()));
+			preparedSt.execute();
+		
+		System.out.println("El libro se ha sido devuelto con exito!");
+	}
+
+	
+	private Prestamo prestamoNoDevuelto(int idLibro) throws SQLException {
+		
+			Prestamo prestamo = new Prestamo();
+	
+			preparedSt = con.prepareStatement("select * from prestamos where id_libro = ? and devuelto = false;");
+			preparedSt.setInt(1, idLibro);
+			ResultSet resultado = preparedSt.executeQuery();
+			if(resultado.next()) {
+				prestamo.setId_libro(idLibro);
+				prestamo.setId_socio(resultado.getInt("id_socio"));
+				prestamo.setFecha(resultado.getDate("fecha"));
+				prestamo.setDevuelto(resultado.getBoolean("devuelto"));
+				
+			}
+			
+			return prestamo;
+
+	}
+
 	
 }
